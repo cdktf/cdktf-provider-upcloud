6088131411cf15a015380ed4908b4dde8e678dbc
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2022-11-28 15:01:32 +0100

    chore(deps): Updated   (#237)
    Triggered by https://github.com/cdktf/cdktf-repository-manager/actions/runs/3565275932

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -71,7 +71,7 @@ export class UpcloudProvider extends cdktf.TerraformProvider {
       terraformResourceType: 'upcloud',
       terraformGeneratorMetadata: {
         providerName: 'upcloud',
-        providerVersion: '2.6.1',
+        providerVersion: '2.7.0',
         providerVersionConstraint: '~> 2.4'
       },
       terraformProviderSource: 'UpCloudLtd/upcloud'
