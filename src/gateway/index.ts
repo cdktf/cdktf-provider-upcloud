77c16a5b245c96398726b13caa2d6d869cd64c7f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-06-06 11:49:34 +0000

    chore(deps): Updated

--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/upcloudltd/upcloud/2.10.0/docs/resources/gateway
 // generated from terraform resource schema
 
