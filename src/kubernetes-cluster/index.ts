9005d31958c0207ff9b7ae247200ff88dd8ac6d8
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-04-16 03:06:57 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/kubernetes-cluster/index.ts
+++ src/kubernetes-cluster/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/upcloudltd/upcloud/5.2.2/docs/resources/kubernetes_cluster
 // generated from terraform resource schema
 
