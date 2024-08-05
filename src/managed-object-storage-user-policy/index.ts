ac2da9dd1d4ec2a2e72f0ee3a6a53b89924368b4
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-05 11:27:09 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/managed-object-storage-user-policy/index.ts
+++ src/managed-object-storage-user-policy/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/upcloudltd/upcloud/5.9.0/docs/resources/managed_object_storage_user_policy
 // generated from terraform resource schema
 
--- src/network-peering/index.ts
+++ src/network-peering/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/upcloudltd/upcloud/5.9.0/docs/resources/network_peering
 // generated from terraform resource schema
 
