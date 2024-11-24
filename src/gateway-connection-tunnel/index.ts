88d37f7bd7d8c438f6e83bb4f0a6b1abdaa3f607
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-24 06:21:40 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@87aa416

--- src/gateway-connection-tunnel/index.ts
+++ src/gateway-connection-tunnel/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/upcloudltd/upcloud/5.15.0/docs/resources/gateway_connection_tunnel
 // generated from terraform resource schema
 
