ec150b275f004f47ddb3a944541b27cb5bfd6bfb
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-01 03:25:57 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-upcloud-managed-object-storage-regions/index.ts
+++ src/data-upcloud-managed-object-storage-regions/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/upcloudltd/upcloud/5.20.1/docs/data-sources/managed_object_storage_regions
 // generated from terraform resource schema
 
