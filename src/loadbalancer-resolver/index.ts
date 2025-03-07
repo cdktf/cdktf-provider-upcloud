bbdbed4db5ec8a075467fce9c36350b138d4a539
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-07 03:25:33 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/loadbalancer-resolver/index.ts
+++ src/loadbalancer-resolver/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/upcloudltd/upcloud/5.20.3/docs/resources/loadbalancer_resolver
 // generated from terraform resource schema
 
