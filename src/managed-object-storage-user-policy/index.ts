0e753bc1f6596d34aacdad2b72a9676d5f112d4c
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-09 06:13:26 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@46eddb5

--- src/managed-object-storage-user-policy/index.ts
+++ src/managed-object-storage-user-policy/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/upcloudltd/upcloud/5.20.3/docs/resources/managed_object_storage_user_policy
 // generated from terraform resource schema
 
