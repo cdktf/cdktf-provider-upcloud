bd1117f99b78c3fd8adcec455b828e11fd656dec
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-09 06:15:47 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-upcloud-load-balancer-dns-challenge-domain/index.ts
+++ src/data-upcloud-load-balancer-dns-challenge-domain/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/upcloudltd/upcloud/5.20.3/docs/data-sources/load_balancer_dns_challenge_domain
 // generated from terraform resource schema
 
